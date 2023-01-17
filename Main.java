9bc35739559ae507f0a13ef8f8b2a5d699952e82
Meedoriya <meedoriya@gmail.com>
2023-01-17 16:58:18 +0500

    second commit

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String separator = File.separator;
+        String path = separator + "Users" + separator + "DELL" + separator + "Desktop" + separator + "input.txt";
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
+
         Scanner sc = new Scanner(System.in);
         try {
             double a = sc.nextDouble();
