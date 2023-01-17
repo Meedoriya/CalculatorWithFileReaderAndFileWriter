08bf52545884dc9dcf1e72af9423a8e9049a8180
Meedoriya <meedoriya@gmail.com>
2023-01-17 19:10:46 +0500

    update

--- Main.java
+++ Main.java
@@ -2,33 +2,36 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         String separator = File.separator;
-        String path = separator + "Users" + separator + "DELL" + separator + "Desktop" + separator + "input.txt";
-        File file = new File(path);
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            System.out.println(scanner.nextLine());
-        }
-        scanner.close();
-
-        Scanner sc = new Scanner(System.in);
-        try {
-            double a = sc.nextDouble();
-            String op = sc.next();
-            double b = sc.nextDouble();
-            sc.close();
-            String ops = "+-*/";
-            if (!ops.contains(op)) System.out.println("Operation Error!");
-            switch(op) {
-                case "/": System.out.println(b == 0 ? "Error! Division by zero" : a / b); break;
-                case "+": System.out.println(a + b); break;
-                case "-": System.out.println(a - b); break;
-                case "*": System.out.println(a * b); break;
+        String path1 = separator + "Users" + separator + "DELL" + separator + "Desktop" + separator + "input.txt";
+        String path2 = separator + "Users" + separator + "DELL" + separator + "Desktop" + separator + "output.txt";
+        File file = new File(path1);
+        Scanner sc1 = new Scanner(file);
+        Scanner sc2 = new Scanner(file);
+        FileWriter fw = new FileWriter(path2);
+        while (sc1.hasNext()) {
+            try {
+                double a = sc1.nextDouble();
+                String op = sc1.next();
+                double b = sc1.nextDouble();
+                String ops = "+-*/";
+                String result = "";
+                if (!ops.contains(op)) result = "Operation Error!";
+                switch (op) {
+                    case "+": result = Double.toString(a + b); break;
+                    case "-": result = Double.toString(a - b); break;
+                    case "*": result = Double.toString(a * b); break;
+                    case "/": result = (b == 0 ? "Error! Division by zero" : Double.toString(a / b)); break;
+                }
+                fw.append(sc2.nextLine() + " = " + result + "\n");
+                fw.flush();
+            } catch (Exception ex) {
+                fw.append(sc2.nextLine() + " = " + "Error! Not number" + "\n");
+                fw.flush();
+                sc1.nextLine();
             }
         }
-        catch (Exception ex) {
-            System.out.println("Error! Not number");
-        }
+        fw.close(); sc1.close(); sc2.close();
     }
 }
